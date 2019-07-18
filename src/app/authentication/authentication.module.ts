1fcf160bfbae606d443e0d54c52f08ead0e0b89d
Ahamad-PC <ahamad.jedu@gmail.com>
2019-07-18 15:47:03 +0700

    Angular6 - 1-6 วางโครงระบบ เพิ่ม SharedsModule.large1080p

--- src/app/authentication/authentication.module.ts
+++ src/app/authentication/authentication.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { AuthRouting } from './authentication.routing';
+import { SharedsModule } from '../shareds/shareds.module';
 
 
 
 @NgModule({
-  declarations: [DashboardComponent],
+  declarations: [
+    DashboardComponent,
+  ],
   imports: [
     AuthRouting,
+    SharedsModule,
     CommonModule
   ]
 })
