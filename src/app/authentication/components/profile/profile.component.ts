8b5e697cda94c78f3c5c080e3f2ab625aa1a78be
Ahamad-PC <ahamad.jedu@gmail.com>
2019-07-21 16:26:47 +0700

    Angular6 - 3-4 ส่วนของหน้า หลังยืนยันตัวตน (สร้างหน้า แก้ไขข้อมูลส่วนตัว เพิ่ม Form data)

--- src/app/authentication/components/profile/profile.component.ts
+++ src/app/authentication/components/profile/profile.component.ts
@@ -1,15 +1,61 @@
 import { Component, OnInit } from '@angular/core';
+import { IProfileComponent } from './profile.interface';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { AccountService } from 'src/app/shareds/services/account.service';
+import { AuthenService } from 'src/app/services/authen.service';
+import { AlertService } from 'src/app/shareds/services/alert.service';
 
 @Component({
-  selector: 'app-profile',
-  templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+    selector: 'app-profile',
+    templateUrl: './profile.component.html',
+    styleUrls: ['./profile.component.css']
 })
-export class ProfileComponent implements OnInit {
+export class ProfileComponent implements IProfileComponent {
+    constructor(
+        private builder: FormBuilder,
+        private account: AccountService,
+        private authen: AuthenService,
+        private alert: AlertService
+    ) {
+        this.initLoadData();
+        this.initialCreateFormData();
+    }
 
-  constructor() { }
+    positionItems: any[] = [
+        'Frontend Development',
+        'Backend Development'
+    ];
+    form: FormGroup;
 
-  ngOnInit() {
-  }
+    // Save Profile
+    onSubmit(): void {
+        //this.form = this.builder.group
+        console.log(this.form.value);
+    }
+
+    private initLoadData() {
+        this.account
+            .getUserLogin(this.authen.getAuthenticated())
+            .then(user => {
+                this.form.controls['email'].setValue(user.email);
+                this.form.controls['firstname'].setValue(user.firstname);
+                this.form.controls['lastname'].setValue(user.lastname);
+                this.form.controls['position'].setValue(user.position);
+                this.form.controls['image'].setValue(user.image);
+            })
+            .catch(err => this.alert.notify(err.Message));
+
+    }
+
+    private initialCreateFormData() {
+        this.form = this.builder.group({
+            email: [''],
+            firstname: [''],
+            lastname: [''],
+            position: [''],
+            image: [''],
+        });
+        this.form.get('email').disable();
+    }
 
 }
